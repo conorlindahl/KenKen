20677a450cce9d67e182815d343b5f6a61f13729
conorlindahl <conorlindahl@utexas.edu>
2015-11-02 18:34:33 -0600

    Separated the solving action from the main.
    This is an attempt to set up the interface that will be used with
    a GUI I want to write. At the moment, I don't know of a way to have
    a main function use JavaFX in an IOC way, so I am having the JavaFX
    GUI interact through an interface with the solver.
    
    I needed to first decouple the solving from my main class. I will
    probably remove the main class soon, as it can be replaced by the
    JavaFX application I plan on writing.

--- src/kenken/Main.java
+++ src/kenken/Main.java
@@ -11,85 +11,9 @@ import java.util.Set;
 
 public class Main {
 
-	private static String fileName = "easySizeFive.txt";
-	
-	public static void main(String[] args) throws IOException, InvalidInitializationException {
+	public static void main(String[] args)  {
 
-		String kenkenString = null; 
-		while(kenkenString == null) {
-			Input i = new InputFromFile();
-			i.specifySource();
-			kenkenString = i.getKenKenString();
-		}
-		
-		Scanner sc = new Scanner(kenkenString);
-		
-		int kenkenSize = Integer.parseInt(sc.nextLine());
-		Grid kenken = new Grid(kenkenSize);
-		int numCages = Integer.parseInt(sc.nextLine());
-
-		while(sc.hasNextLine()) {
-			String cage = sc.nextLine();
-			kenken.addCage(cage);
-		}
-		
-		List<Grid.Cage> cages = kenken.getCages();
-		
-		boolean rightNumCages = cages.size() == numCages;
-		boolean fullyInitialized = kenken.isInitialized();
-		
-		if(!rightNumCages || !fullyInitialized) {
-			throw new InvalidInitializationException(
-					"\nRight number of cages: " + rightNumCages + "\n"
-					+ "Fully Initialized: " + fullyInitialized);
-		}
-		
-		if ( recurseSolve(0, cages, kenken) ) {
-			kenken.print();
-		} else {
-			System.out.println("No solution");
-		}
 		
 		
-	}
-	
-	private static boolean recurseSolve(int cageNumber, List<Grid.Cage> cages, Grid grid) {
-		if( cageNumber == cages.size() ) {
-			// Verify grid is valid
-			return grid.isFilled();
-		}
-		
-		Grid.Cage current = cages.get(cageNumber);
-		
-		/* Step 1: get the options for cages */
-		Set<int[]> options = current.getOptions();
-		
-		/* Step 2: choose next option to test 
-		 *          if out of steps -> step 4*/
-		for(int[] i : options) {
-			current.fillValues(i);
-			/* Step 3: if test works -> return true
-			 *         else -> Step 2
-			 */
-			if ( !grid.isValid() ) { // If grid breaks, don't want this value
-				continue;
-			}
-			
-			if (recurseSolve(cageNumber+1, cages, grid) ) {
-				return true;
-			}
-		}
-		
-		/* Step 4: reset squares & return false
-		 */
-		current.reset();
-		return false;
-	}
-
-}
-
-class InvalidInitializationException extends Exception {
-	public InvalidInitializationException(String a) {
-		super(a);
 	}
 }
