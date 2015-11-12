81c0568b3dd1182c4bd1f459c8cc2a73a930c1e6
conorlindahl <conorlindahl@utexas.edu>
2015-11-12 02:23:06 -0600

    Finished beginning work on grid visuals, began controls.
    The grid visuals will likely be a work in progress throughout this
    project because as I add functionality, I'll need to update the
    visuals. So I have the basics right now; The grid is always a 5x5
    grid and you can click and unclick the Squares.
    
    I added an inner class called VisualCage that will be used to
    represent Cages once I move on to that functionality.
    
    I began work on the control panel for the solver gui. I currently
    have a drop down selection to choose an operation and a spot to
    enter a number for the Cage. Next I will add a button that allows
    a user to create a Cage out of the selected Squares.

--- src/gui/KenKenApp.java
+++ src/gui/KenKenApp.java
@@ -1,6 +1,8 @@
 package gui;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javafx.application.Application;
@@ -10,18 +12,27 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class KenKenApp extends Application {
 
+	private List<VisualSquare[]> lockedCages;
+	
+	{
+		lockedCages = new ArrayList<VisualSquare[]>();
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
@@ -35,11 +46,17 @@ public class KenKenApp extends Application {
 		
 		GridPane grid = setUpKenKenVisual();
 		
+		VBox controlPanel = getControlPanel();
+		
 		BorderPane layout = new BorderPane();
 		layout.setTop(title);
 		BorderPane.setAlignment(title, Pos.BOTTOM_CENTER);
 		BorderPane.setMargin(title, new Insets(20, 0, 0, 0));
 		layout.setLeft(grid);
+		layout.setRight(controlPanel);
+		BorderPane.setAlignment(controlPanel, Pos.BOTTOM_LEFT);
+		BorderPane.setMargin(controlPanel, new Insets(50, 100, 0, 0));
+
 		Scene r = new Scene(layout, 550, 400);
 		
 		primaryStage.setResizable(false);
@@ -64,14 +81,17 @@ public class KenKenApp extends Application {
 						if(v.locked) { return; }
 						
 						GraphicsContext gc = v.getGraphicsContext2D();
-						if (v.selected) {
-							gc.setStroke(Color.DARKGRAY);
-							gc.setLineWidth(2.0);
+						if (!v.selected) {
+							gc.setStroke(Color.BLACK);
+							gc.setLineWidth(4.0);
 							gc.strokeRect(0, 0, 50, 50);
 							VisualSquare.selectedSquares.remove(v);
 						} else {
-							gc.setStroke(Color.BLACK);
-							gc.setLineWidth(2.0);
+							gc.setStroke(Color.WHITE);
+							gc.setLineWidth(5.0);
+							gc.strokeRect(0, 0, 50, 50);
+							gc.setStroke(Color.DARKGREY);
+							gc.setLineWidth(3.0);
 							gc.strokeRect(0, 0, 50, 50);
 							VisualSquare.selectedSquares.add(v);
 						}
@@ -80,8 +100,8 @@ public class KenKenApp extends Application {
 					
 				});
 				GraphicsContext gc = v.getGraphicsContext2D();
-				gc.setStroke(Color.DARKGRAY);
-				gc.setLineWidth(2.0);
+				gc.setStroke(Color.DARKGREY);
+				gc.setLineWidth(3.0);
 				gc.strokeRect(0, 0, 50, 50);
 				grid.add(v, x, y);
 			}
@@ -89,13 +109,82 @@ public class KenKenApp extends Application {
 		
 		return grid;
 	}
+	
+	private VBox getControlPanel() {
+		VBox container = new VBox();
+		container.setPadding(new Insets(5,0,0,5));
+		container.setSpacing(5);
+		
+		/*
+		 * The drop down box for choosing what operation a cage should be
+		 */
+		ChoiceBox<String> operationSelection = new ChoiceBox<String>();
+		operationSelection.setMaxWidth(150);
+		String mult = "Multiply";
+		String add = "Add";
+		String sub = "Subtract";
+		String div = "Divide";
+		String eq = "Equal";
+		operationSelection.getItems().addAll(add, sub, mult, div, eq);
+		operationSelection.setValue(add);
+		
+		container.getChildren().add(operationSelection);
 
+		/*
+		 * The resource to determine the value for a specific cage
+		 */
+		TextField totalInput = new TextField();
+		
+		HBox totalBox = new HBox();
+		Label totalLabel = new Label("Cage Total: ");
+		totalLabel.setPadding(new Insets(5, 0, 0, 0));
+		totalInput.setMaxWidth(50);
+		totalBox.getChildren().addAll(totalLabel, totalInput);
+		
+		/*
+		 * The resource that activates the creation of the Cages
+		 */
+		
+		container.getChildren().add(totalBox);
+		return container;
+	}
+
+	private static class VisualCage {
+		List<VisualSquare> containedSquares;
+		String operation;
+		int total;
+		
+		public VisualCage(String op, int t) {
+			containedSquares = new ArrayList<VisualSquare>();
+		}
+		
+		public void addSquares(List<VisualSquare> toAdd) {
+			for(VisualSquare i : toAdd) {
+				containedSquares.add(i);
+			}
+		}
+		
+		/**
+		 * Makes String of the form:
+		 * <operation> <total> <# of Squares> <coordinate points of Squares>
+		 */
+		public String toString() {
+			String rep = operation;
+			rep += " " + total + " " + containedSquares.size();
+			for(VisualSquare s : containedSquares) {
+				rep += " (" + s.row + "," + s.col + ")";
+			}
+			
+			return rep;
+		}
+	}
 	
 	private static class VisualSquare extends Canvas {
 		int row;
 		int col;
 		boolean selected;
 		boolean locked;
+		//You're my boolean and I have you locked ;)
 		
 		public VisualSquare() {
 			genericConstruction();
@@ -110,6 +199,7 @@ public class KenKenApp extends Application {
 			row = 0;
 			col = 0;
 			selected = false;
+			locked = false;
 		}
 		
 		static Set<VisualSquare> selectedSquares;
